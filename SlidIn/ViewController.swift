16ca90cb183b978adcf4fb6e6f1fcf00e5957aa0
hongky <lienghongky.planb@gmail.com>
2018-08-19 10:37:27 +0700

    Initial Commit

---
+++ SlidIn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidIn
+//
+//  Created by Lieng Hongky on 8/19/18.
+//  Copyright © 2018 Lieng Hongky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
